bfebc0f7733afe60750e40b084b7a35d0e55f0b7
lauhei <lauri.heikkinen5@metropolia.fi>
2024-04-14 20:42:21 +0300

    build

--- src/views/Home.tsx
+++
@@ -1,9 +0,0 @@
-function Home() {
-	return (
-		<div className="w-full h-full absolute left-0 bg-gray-800 flex items-center justify-center">
-			<h1 id="home-title">SSSF Project</h1>
-		</div>
-	);
-}
-
-export default Home;
